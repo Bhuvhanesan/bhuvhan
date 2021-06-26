a2674ee42175af0ea097af84f9ee6c38933f2dd8
Bhuvhanesan <82365536+Bhuvhanesan@users.noreply.github.com>
2021-06-26 16:46:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function compute() {
+    p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+    a = document.getElementById("result").value;
+    document.getElementById("word").innerHTML = "You will receive an total amount of "; 
+    document.getElementById("result").innerHTML = p * (1 + (r/100) * t);
+    document.getElementById("message").innerHTML = "in the year ";
+    const currentYear = new Date();
+    document.getElementById("current").innerHTML = parseInt(t) + (currentYear.getFullYear());
+}
